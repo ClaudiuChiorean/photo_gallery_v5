cdd4c98d4284445fe6d7d2f410ae83ae833e00bf
ClaudiuChiorean <claudiuchiorean2@gmail.com>
2023-08-04 13:20:39 +0300

    Update files
    1. csss\styles.css
    2. index.html
    3. js\app.js
    4. js\searchFilter.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 console.log('test');
+baguetteBox.run('.album');
+
+// Custom filter
+
+const searchBar = document.getElementById(`search`);
+searchBar.addEventListener('keyup', e => {
+    let searchValue = e.target.value.toLowerCase();
+    let photo = document.querySelectorAll('a.box-photo');
+    photo.forEach(photo => {
+        let dataCap = photo.getAttribute('data-caption');
+        if (dataCap.toLowerCase().includes(searchValue)) {
+            photo.style.display = 'grid';
+        } else {
+            photo.style.display = 'none';
+        }
+    });
+});
